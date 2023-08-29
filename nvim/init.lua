5c808d64c4ac54766900084cd1589c2277011ce4
dmighty007 <dibyendumaity1999@bose.res.in>
2023-08-30 03:08:24 +0530

    Astrovim configured

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,38 @@
-require "core"
+if vim.loader and vim.fn.has "nvim-0.9.1" == 1 then vim.loader.enable() end
+-- set shorter name for keymap function
+local kmap = vim.keymap.set
 
-local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
+-- F5 processes the document once, and refreshes the view
+-- kmap({ 'n', 'v', 'i' },'<F3>', function() require("knap").process_once() end)
 
-if custom_init_path then
-  dofile(custom_init_path)
+-- F6 closes the viewer application, and allows settings to be reset
+-- kmap({ 'n', 'v', 'i' },'<F6>', function() require("knap").close_viewer() end)
+
+-- F7 toggles the auto-processing on and off
+kmap({ 'n', 'v', 'i' },'<F2>', function() require("knap").toggle_autopreviewing() end)
+
+-- F8 invokes a SyncTeX forward search, or similar, where appropriate
+-- kmap({ 'n', 'v', 'i' },'<F8>', function() require("knap").forward_jump() end)
+for _, source in ipairs {
+  "astronvim.bootstrap",
+  "astronvim.options",
+  "astronvim.lazy",
+  "astronvim.autocmds",
+  "astronvim.mappings",
+} do
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
 end
 
-require("core.utils").load_mappings()
-
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
--- bootstrap lazy.nvim!
-if not vim.loop.fs_stat(lazypath) then
-  require("core.bootstrap").gen_chadrc_template()
-  require("core.bootstrap").lazy(lazypath)
+if astronvim.default_colorscheme then
+  if not pcall(vim.cmd.colorscheme, astronvim.default_colorscheme) then
+    require("astronvim.utils").notify(
+      ("Error setting up colorscheme: `%s`"):format(astronvim.default_colorscheme),
+      vim.log.levels.ERROR
+    )
+  end
 end
 
-dofile(vim.g.base46_cache .. "defaults")
-vim.opt.rtp:prepend(lazypath)
-require "plugins"
+require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, false), true)
+require("plugins.configs.lualine-config")
+
